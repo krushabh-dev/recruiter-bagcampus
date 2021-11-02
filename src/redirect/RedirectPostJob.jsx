ea38e1ec7e6ad445d174f96d26bcbc00f1229e7a
Krushabhsingh Suryawanshi <71483683+krushabh-dev@users.noreply.github.com>
2021-11-02 13:22:13 +0530

    Update RedirectPostJob.jsx

--- src/redirect/RedirectPostJob.jsx
+++ src/redirect/RedirectPostJob.jsx
@@ -38,7 +38,7 @@ class RedirectPostJob extends Component {
         {(() => {
           if (this.state.user) {
             if (this.state.user.emailVerified == false) {
-              return window.location.replace("http://localhost:3000/#/evs");
+              return window.location.replace("https://dashboard.bagcampus.in/#/evs");
             }
           }
         })()}
